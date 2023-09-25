a13b9594b5384d7bbc8b278482ac0df1553510e9
AhmadSalem178 <84476551+AhmadSalem178@users.noreply.github.com>
2023-09-25 21:53:14 +0300

    Add files via upload

---
+++ login/script.js
@@ -0,0 +1,14 @@
+
+
+let username = prompt("what is your name?")
+
+alert("welcome to the page , " + username)
+
+let age = prompt("How old are you?")
+
+if(age>18){
+    console.log("you are adult")
+}
+else{
+    console.log("you are not an adult")
+}
